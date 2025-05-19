b85663b2dce22e576652faa86e868bdca23e515f
Rogersovich <53912205+rogersovich@users.noreply.github.com>
2025-05-19 12:25:44 +0700

    remove console log

--- pages/project/index.vue
+++ pages/project/index.vue
@@ -179,10 +179,6 @@ const { data: dataProjects, pending } = await useAsyncData(
   "publicProjects",
   async () => {
     try {
-      console.log(BASE_API)
-      console.log({
-        waduh: runtimeConfig.public
-      })
       const response = await $fetch<TPublicProjectListResponse>(
         `${BASE_API}/api-public/projects`,
         {
