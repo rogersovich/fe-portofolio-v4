6eaac25e6090c6ffca1204657b10243c7a6c804d
Rogersovich <53912205+rogersovich@users.noreply.github.com>
2025-05-23 21:10:51 +0700

    Refactor index.vue to use setup script and implement SEO metadata; add new image for enhanced portfolio presentation.

--- pages/index.vue
+++ pages/index.vue
@@ -3,16 +3,15 @@
     <BaseHeroComponent />
   </div>
 </template>
-<script>
-export default {
-  setup() {
-    useHead({
-      title: 'Home',
-      titleTemplate: '%s | Portofolio'
-    })
-  }
-}
+<script setup lang="ts">
+const siteUrl = 'https://dimasroger.com' 
+useSeoMeta({
+  title: 'Home | Portofolio',
+  ogTitle: 'Home | Portofolio',
+  description: 'Hello, my name is Dimas Roger Widianto, please enjoy my portofolio',
+  ogDescription: 'Hello, my name is Dimas Roger Widianto, please enjoy my portofolio',
+  ogImage: `${siteUrl}/images/1-1200x650.jpg`,
+  twitterCard: 'summary_large_image',
+})
 </script>
-<style lang="">
-  
-</style>
+<style lang=""></style>
