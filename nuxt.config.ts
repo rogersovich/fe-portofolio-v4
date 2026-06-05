a30b0c6d8e6633ca45024df03215ed16ed914906
Rogersovich <53912205+rogersovich@users.noreply.github.com>
2026-06-05 09:45:08 +0700

    chore: remove MinIO config from FE

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,8 +20,6 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       apiBase: process.env.NUXT_PUBLIC_API_BASE,
-      minio_endpoint: process.env.NUXT_PUBLIC_MINIO_ENDPOINT,
-      minio_bucket: process.env.NUXT_PUBLIC_MINIO_BUCKET,
     },
   },
   plugins: [],
